58530b1f0ffe277e961c45b7122687b75f5df640
taohuu <98674519+taohuu@users.noreply.github.com>
2023-03-31 12:17:05 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'MY-BLOG-aa33cf7c6a194637a37ba3e066692cd8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Taohuu',
+  domain: 'https://www.notion.so/MY-BLOG-aa33cf7c6a194637a37ba3e066692cd8',
+  author: 'henryxu',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
